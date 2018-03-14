12a342129fd2b7efe0c8bcabfac819eeb4826a92
Max Wofford <max@maxwofford.com>
2018-03-14 09:48:48 -0700

    Add favicon

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,17 +3,19 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { ThemeProvider } from '@hackclub/design-system'
 
+import favicon from 'static/favicon.ico'
+
 const TemplateWrapper = ({ children }) => (
   <ThemeProvider>
-    <Helmet
-      title={`${new Date().getFullYear()} High School Hackathon Calendar // Worldwide`}
-      meta={[
-        {
-          name: 'description',
-          content: 'Community contributed high school hackathon schedule',
-        },
-      ]}
-    />
+    <Helmet>
+      <title
+      >{`${new Date().getFullYear()} High School Hackathon Calendar // Worldwide`}</title>
+      <meta
+        name="description"
+        content="Community contributed high school hackathon schedule"
+      />
+      <link rel="icon" href={favicon} />
+    </Helmet>
     {children()}
   </ThemeProvider>
 )
