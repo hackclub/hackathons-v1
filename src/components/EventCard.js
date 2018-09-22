613a807784dbba9a61df4048ade6468ef2a6b2e0
Zane <github@zane.sh>
2018-09-21 19:48:20 -0700

    Revert "Only load MLH logo when needed"

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -62,6 +62,7 @@ const MLHLogo = styled(Image).attrs({
 })`
   border-top-right-radius: ${({ theme }) => theme.radius};
   border-bottom-right-radius: ${({ theme }) => theme.radius};
+  visibility: ${props => (props.mlhAssociated ? 'visible' : 'hidden')};
 `
 
 const AssociatedSeal = styled(Box).attrs({
@@ -70,11 +71,11 @@ const AssociatedSeal = styled(Box).attrs({
   mt: -theme.space[3] + 5,
   mb: 1,
   children: props => (
-    <>
-      {props.mlhAssociated && (
-        <MLHLogo src="/mlh-logo-grayscale.svg" alt="MLH logo" />
-      )}
-    </>
+    <MLHLogo
+      src="/mlh-logo-grayscale.svg"
+      alt="MLH logo"
+      mlhAssociated={props.mlhAssociated}
+    />
   ),
 })``
 
