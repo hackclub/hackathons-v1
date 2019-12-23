d87b9261f633d1f5bb530334259e77b87c47db36
Feross Aboukhadijeh <feross@feross.org>
2019-12-22 17:12:36 -0800

    Fix eslint warning

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,33 +33,33 @@ const HideOnMobile = styled(Box)`
   }
 `
 
-const Switch = styled(Box)`
-  border-radius: 99999px;
-  display: inline-flex;
-  width: 40px;
-  height: 24px;
-  background-color: ${props =>
-    props.checked ? props.theme.cx(props.color) : 'transparent'};
-  box-shadow: inset 0 0 0 2px;
-  transition-property: background-color;
-  transition-duration: 0.25s;
-  transition-timing-function: ease-out;
-  user-select: none;
-  &:after {
-    content: ' ';
-    width: 16px;
-    height: 16px;
-    margin: 4px;
-    border-radius: 8px;
-    background-color: ${props =>
-      props.checked ? props.theme.colors.white : props.theme.cx(props.color)};
-    transition-property: transform, color;
-    transition-duration: 0.125s;
-    transition-timing-function: ease-out;
-    transform: ${props =>
-      props.checked ? `translateX(16px)` : `translateX(0)`};
-  }
-`
+// const Switch = styled(Box)`
+//   border-radius: 99999px;
+//   display: inline-flex;
+//   width: 40px;
+//   height: 24px;
+//   background-color: ${props =>
+//     props.checked ? props.theme.cx(props.color) : 'transparent'};
+//   box-shadow: inset 0 0 0 2px;
+//   transition-property: background-color;
+//   transition-duration: 0.25s;
+//   transition-timing-function: ease-out;
+//   user-select: none;
+//   &:after {
+//     content: ' ';
+//     width: 16px;
+//     height: 16px;
+//     margin: 4px;
+//     border-radius: 8px;
+//     background-color: ${props =>
+//       props.checked ? props.theme.colors.white : props.theme.cx(props.color)};
+//     transition-property: transform, color;
+//     transition-duration: 0.125s;
+//     transition-timing-function: ease-out;
+//     transform: ${props =>
+//       props.checked ? `translateX(16px)` : `translateX(0)`};
+//   }
+// `
 
 const SectionHeading = styled(Heading.h2).attrs({
   f: [4, 5],
